5b83ab87ef0d4311a559a253cd275852000d4661
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-13 20:51:05 +0300

    Import HttpClient module

--- core/Giphy-Api/src/app/app.module.ts
+++ core/Giphy-Api/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { GifsComponent } from './gifs/gifs.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
--- core/Giphy-Api/src/environments/environment.ts
+++ core/Giphy-Api/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey : "uwDNo4T8y8zFAOBZplPWY97vWIe4i4pu"
 };
 
 /*

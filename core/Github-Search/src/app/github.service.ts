368fb9bb272ff9946d744dd7461c46cf64403fbc
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-14 21:03:32 +0300

    import httpclientmodule

--- core/Github-Search/src/app/github.service.ts
+++ core/Github-Search/src/app/github.service.ts
@@ -1,9 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(http:HttpClientModule) { }
 }

33e9857a0286eec71b2522aff535626150cd347b
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-15 11:46:41 +0300

    assign response to user

--- core/Github-Search/src/app/profile/profile.component.ts
+++ core/Github-Search/src/app/profile/profile.component.ts
@@ -7,12 +7,14 @@ import { GithubService } from '../github.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user:any = [];
 
   constructor(private githubService:GithubService) { }
 
   ngOnInit(): void {
     this.githubService.getUserProfile().subscribe((response:any) => {
       console.log(response)
+      this.user = response;
     })
   }
 
